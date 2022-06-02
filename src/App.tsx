388b11764683f0c1f9a2f9d73f1db8035462bbd9
spencer.allegrezza@staplcotn.com <sallegrezz@gmail.com>
2022-06-02 18:54:02 -0500

    Add water.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 const App = () => {
-  return <h1>Hello workl;s</h1>;
+  return (
+    <>
+      <div className="name-box">
+        <h1>Spencer Allegrezza's website</h1>
+        <img src="src/under_construction.gif" height="50px" width="50px" />
+      </div>
+      <hr />
+      <section style={{ boxShadow: "0px 0px 2px rgba(17, 17, 17, 0.25)" }}>
+        <h2>Professional works</h2>
+        <h1> Staplcotn Members</h1>
+      </section>
+    </>
+  );
 };
 
-export default App
+export default App;
