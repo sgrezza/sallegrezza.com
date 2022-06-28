396e1441c3481eaceabf703738a14396768c0751
spencer.allegrezza@staplcotn.com <sallegrezz@gmail.com>
2022-06-27 21:38:27 -0500

    Some more work

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ const App = () => {
     <>
       <div className="name-box">
         <h1>Spencer Allegrezza's website</h1>
-        <img src="src/under_construction.gif" height="50px" width="50px" />
+        <a href="https://github.com/sgrezza" className="link with-icon">
+          <img
+            src="src/assets/Github-Mark-120px-plus.png"
+            alt="Github logo"
+            className="icon"
+          />
+          My Github
+        </a>
+        <a href="mailto: spencer@sallegrezz.com" className="link with-text">
+          <p>Send me an email</p>
+        </a>
       </div>
       <hr />
-      <section style={{ boxShadow: "0px 0px 2px rgba(17, 17, 17, 0.25)" }}>
+      <section className="section-box">
+        <h2>About</h2>
+        <p>Full-stack dev by day, full-stack dev by night.</p>
+      </section>
+      <section className="section-box">
         <h2>Professional works</h2>
-        <h1> Staplcotn Members</h1>
+        {/* <h1> Staplcotn Members</h1> */}
+        <img src="src/under_construction.gif" height="50px" width="50px" />
       </section>
     </>
   );
